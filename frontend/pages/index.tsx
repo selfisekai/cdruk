b156cfa54ebf82743a095eed187977108782cbf5
selfisekai <today@selfisekai.rocks>
2020-04-04 16:06:24 +0200

    frontend: basic start page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import Footer from '../components/footer'
 import Menu from '../components/menu'
 
@@ -8,11 +9,23 @@ function Index() {
       <Menu />
       <section className="section">
         <div className="container">
-          <h1 className="title">Section</h1>
-          <h2 className="subtitle">
-            A simple container to divide your page into{' '}
-            <strong>sections</strong>, like the one you're currently reading
+          <h1 className="title is-3">cdruk</h1>
+          <h2 className="subtitle is-4">
+            Lekarzom w całej Polsce brakuje sprzętu. Potrzebna jest im teraz pomoc, aby mogli pomagać nam.
           </h2>
+          <p className="subtitle is-4 has-text-weight-semibold">
+            Masz drukarkę 3D? {' '}
+            <Link href="/signup">
+              <a className="button is-inline is-primary">Dołącz do akcji</a>
+            </Link> {' '}
+            i drukuj sprzęt dla pobliskich szpitali.
+          </p>
+          <p className="subtitle is-4 has-text-weight-semibold">
+             W twoim szpitalu brakuje sprzętu? {' '}
+             <Link href="/signup">
+              <a className="button is-inline is-primary is-light">Zgłoś się do nas tutaj!</a>
+            </Link>
+          </p>
         </div>
       </section>
       <Footer />
