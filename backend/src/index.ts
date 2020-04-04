636057d99175ea8b3ee4350b8973d90a27e1fe38
ptrcnull <git@ptrcnull.me>
2020-04-04 15:53:48 +0200

    backend: Added connecting to the database
    Also, fixed some problems with TypeORM

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,53 @@
-import Koa from 'koa';
-import Router from 'koa-router';
-import bodyParser from 'koa-bodyparser';
+import 'reflect-metadata'
+import Koa from 'koa'
+import Router from 'koa-router'
+import bodyParser from 'koa-bodyparser'
+import { Connection, createConnection } from 'typeorm'
+import { User } from './entity/User'
 
-const koa = new Koa();
-const router = new Router();
+let db: Connection
+
+const koa = new Koa()
+const router = new Router()
 koa
   .use(bodyParser())
   .use(router.routes())
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
 
 router
   .get('/', (ctx) => {
     ctx.body = {
-      data: 'hello world!',
-    };
+      data: 'hello world!'
+    }
   })
   .post('/register', (ctx) => {
-    ctx.body = ctx.request.body;
+    const repo = db.getRepository(User)
+    ctx.body = ctx.request.body
+
   })
   .post('/login', (ctx) => {
-    ctx.body = ctx.request.body;
+    ctx.body = ctx.request.body
   })
   .post('/getObjects', (ctx) => {
-    ctx.body = ctx.request.body;
+    ctx.body = ctx.request.body
   })
   .post('/getMerchants', (ctx) => {
-    ctx.body = ctx.request.body;
+    ctx.body = ctx.request.body
   })
   .post('/getMerchantQueue', (ctx) => {
-    ctx.body = ctx.request.body;
+    ctx.body = ctx.request.body
   })
   .post('/order', (ctx) => {
-    ctx.body = ctx.request.body;
+    ctx.body = ctx.request.body
   })
   .post('/getOrder', (ctx) => {
-    ctx.body = ctx.request.body;
-  });
+    ctx.body = ctx.request.body
+  })
 
-koa.listen(3000);
+createConnection()
+  .then((conn: Connection) => {
+    db = conn
+  })
+  .then(() => koa.listen(3000))
+  // eslint-disable-next-line no-console
+  .then(() => console.log('Listening on 0.0.0.0:3000'))
