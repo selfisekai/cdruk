ded07aa733f2ba1db3cf8ac9cec42468c78082df
xdk78 <xdk78888@gmail.com>
2020-04-04 23:09:54 +0200

    fix(frontend): remove withCredentials

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -5,4 +5,4 @@ const BASE_URL = process.env.BASE_URL || 'http://localhost:3001'
 axios.defaults.baseURL = BASE_URL
 
 export default (endpoint: string, config: AxiosRequestConfig) =>
-  axios(endpoint, Object.assign({ withCredentials: true }, config))
+  axios(endpoint, config)
